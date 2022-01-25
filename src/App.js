4c9dd805ec155cd6755b286216ecc48d2d49dbba
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-01-24 21:01:38 -0500

    added the hero video container for mobile view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import VideoDetails from "./components/VideoList/VideoDetails";
 import Header from "./components/Header";
+import HeroVideo from "./components/HeroVideo";
 
 function App() {
   return (
     <div>
       <Header />
+      <HeroVideo />
       <VideoDetails />
     </div>
   );
---
+++ src/components/HeroVideo.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "../styles/hero-container.scss";
+
+export default class HeroVideo extends Component {
+  render() {
+    return (
+      <div className="Hero-container__main">
+        image: "https://i.imgur.com/l2Xfgpl.jpg",
+        <video
+          className="Hero-container__main-video"
+          controls
+          poster="https://i.imgur.com/l2Xfgpl.jpg"
+        >
+          No Support for Hero Image
+        </video>
+      </div>
+    );
+  }
+}
