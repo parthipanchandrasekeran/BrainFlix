ceba325534aa0233dd2b82bd1ad140294c253d7b
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-01-31 20:35:14 -0500

    fixed the bug for side video width on mobile

--- src/components/VideoDetails/dateFormatter.js
+++ src/components/VideoDetails/dateFormatter.js
@@ -1,12 +1,6 @@
 function dateFormatter(dateFormated) {
   const commentDate = new Date(dateFormated);
-
-  let month = commentDate.getMonth() + 1;
-  let day = commentDate.getDate();
-  const year = commentDate.getFullYear();
-
   const todaysDate = new Date();
-  //let todaysDateDay=todaysDate.getDate();
   const diff = Math.abs(todaysDate - commentDate);
   const days = Math.round(diff / (1000 * 3600 * 24));
 
