164e691d1e2889440012b435e837a124f7f1bc47
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-02-01 20:01:30 -0500

    added the video element aspect ratio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
-import VideoInfo from "./components/VideoInfo";
+import VideoInfo from "./components/MainSection";
 
 function App() {
   return (
