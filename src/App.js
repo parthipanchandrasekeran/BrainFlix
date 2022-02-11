d145170ec9d54d9684ffe9c863a2a73359938a15
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-02-10 22:25:38 -0500

    added the upload page link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
 import Header from "./components/Header";
+import UploadPage from "./components/UploadPage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Component } from "react";
 
@@ -12,6 +13,7 @@ export default class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/HomePage" component={HomePage} />
+          <Route path="/UploadPage" component={UploadPage} />
         </Switch>
       </Router>
     );
---
+++ src/components/UploadPage.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export default class UploadPage extends Component {
+  render() {
+    return <h1>UploadPage</h1>;
+  }
+}
