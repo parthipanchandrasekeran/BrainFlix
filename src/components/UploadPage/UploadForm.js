4908f54cc387ef9a6e206cfd40178cace53cc921
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-02-21 19:39:31 -0500

    added the form submission using states on upload page

--- src/components/UploadPage/UploadForm.js
+++ src/components/UploadPage/UploadForm.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 
 export class UploadForm extends Component {
+  state = {
+    title: "",
+    description: "",
+  };
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+    alert("submitted successfully");
+  }
+
   render() {
     return (
-      <form className="upload-form">
+      <form
+        onSubmit={(event) => {
+          this.handleSubmit(event);
+        }}
+        className="upload-form"
+      >
         <p className="upload-form__label-title">TITLE YOUR VIDEO</p>
         <div className="upload-form__input-title-container">
           <input
+            onChange={(event) => {
+              this.onChange(event);
+            }}
             className="upload-form__input-title"
+            name="title"
             placeholder="Add a title to your video"
           ></input>
         </div>
@@ -16,12 +40,18 @@ export class UploadForm extends Component {
         </p>
         <div className="upload-form__input-description-container">
           <input
+            onChange={(event) => {
+              this.onChange(event);
+            }}
+            name="description"
             className="upload-form__input-description"
             placeholder="Add a description to your video"
           ></input>
         </div>
         <div className="upload-form__button-container-main">
-          <button className="upload-form__publish-button">PUBLISH</button>
+          <button type="submit" className="upload-form__publish-button">
+            PUBLISH
+          </button>
           <button className="upload-form__cancel-button">CANCEL</button>
         </div>
       </form>
