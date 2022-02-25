bfe0e1919339339a851ddd68e47726ce236accb8
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-02-24 19:29:13 -0500

    fixed the upload page hero image issue for desktop view

--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -8,17 +8,24 @@ export default class UploadPage extends Component {
     return (
       <>
         <h1 className="upload-page-header">Upload Video</h1>
-        <section className="upload-page__hero-section">
-          <p className="upload-page__hero-section-title">VIDEO THUMBNAIL</p>
-          <div className="upload-page__hero-section-image-container">
-            <img
-              className="upload-page__hero-section-image"
-              src={UploadVideoImage}
-              alt="upload-hero-image"
-            />
+        <div className="upload-page-header__desktop-main">
+          <section className="upload-page__hero-section">
+            <p className="upload-page__hero-section-title">VIDEO THUMBNAIL</p>
+            <div className="upload-page__hero-section-image-container">
+              <img
+                className="upload-page__hero-section-image"
+                src={UploadVideoImage}
+                alt="upload-hero-image"
+              />
+            </div>
+          </section>
+          <div className="upload-page__desktop-main">
+            <UploadForm match={this.props} />
           </div>
-        </section>
-        <UploadForm match={this.props} />
+        </div>
+        <div className="upload-page__mobile-main">
+          <UploadForm match={this.props} />
+        </div>
       </>
     );
   }
