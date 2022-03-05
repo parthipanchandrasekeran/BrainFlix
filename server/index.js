dce2f1c8ac676810a4daba725e5abece4006d69d
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-03-05 16:19:41 -0500

    added the env variable file

--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 4000;
-
+const { PORT, BACKEND_URL } = process.env;
+console.log(PORT);
 const videosRoute = require("./routes/videos");
 
 app.use(express.static("public"));
+app.use("/static", express.static("public/Upload-video-preview.jpg"));
 
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
   const value = req.url.replace("/", "");
 
-  if (value === "static") {
+  if (value !== "static") {
     console.log(value);
     next();
   } else {
     res.status(404).send("incorrect");
   }
-});*/
+});
 
 app.use(cors());
 
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -2,6 +2,7 @@ let videoDetails = [];
 let videos = [];
 const express = require("express");
 const fs = require("fs");
+const router = express.Router();
 
 fs.readFile("./data/video-details.json", "utf-8", (err, data) => {
   if (err) throw err;
@@ -15,7 +16,19 @@ fs.readFile("./data/videos.json", "utf-8", (err, data) => {
   videos = JSON.parse(data);
 });
 
-const router = express.Router();
+router.get("/videos", (_, res) => {
+  res.status(202).send(videos);
+});
+
+router.get("/videos/:videoID", (req, res) => {
+  const mainVideoDetails = videoDetails.find((video) => {
+    return video.id === req.params.videoID;
+  });
+
+  mainVideoDetails
+    ? res.status(202).send(mainVideoDetails)
+    : res.status(404).send("no luck");
+});
 
 const soup = ["tomato soup", "sweet corn", "manchow", "lobstar tail"];
 const salad = [
@@ -24,21 +37,6 @@ const salad = [
   "manchow salad",
   "lobstar tail salad",
 ];
-
-router.get("/videos", (req, res) => {
-  res.status(202).send(videos);
-});
-
-router.get("/videos/:videoID", (req, res) => {
-  const mainVideoDetails = videoDetails.find((video) => {
-    return video.id === req.params.videoID;
-  });
-  if (mainVideoDetails) {
-    res.status(202).send(mainVideoDetails);
-  } else {
-    res.status(404).send("no luck");
-  }
-});
 
 router.get("/famousdish", (req, res) => {
   res.status(202).send("famous dish is surf and tail lobster");
