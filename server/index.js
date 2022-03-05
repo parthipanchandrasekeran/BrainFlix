91be2afdc3c2f334c5f63a40f4724ddc637b0ce9
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2022-03-05 15:08:29 -0500

    added the json files to the data section and populating the data through fs

--- server/index.js
+++ server/index.js
@@ -5,6 +5,19 @@ const PORT = 4000;
 
 const videosRoute = require("./routes/videos");
 
+app.use(express.static("public"));
+
+/*app.use((req, res, next) => {
+  const value = req.url.replace("/", "");
+
+  if (value === "static") {
+    console.log(value);
+    next();
+  } else {
+    res.status(404).send("incorrect");
+  }
+});*/
+
 app.use(cors());
 
 app.use("/", videosRoute);
